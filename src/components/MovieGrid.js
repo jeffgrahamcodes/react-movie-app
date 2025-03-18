9eaba53aa9851f19b3d81b73a8bff60fdc951534
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-18 14:55:34 -0400

    create search bar for filtering

--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -4,6 +4,7 @@ import MovieCard from './MovieCard';
 
 export default function MovieGrid() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch('movies.json')
@@ -11,11 +12,28 @@ export default function MovieGrid() {
       .then((data) => setMovies(data));
   }, []);
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="movies-grid">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </div>
+    <>
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search movies.."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </>
   );
 }
