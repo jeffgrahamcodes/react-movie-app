83023ec2465ccb0cc48140f688ba90bc1ddc4d72
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-18 15:05:25 -0400

    create dropdowns for filtering

--- src/components/MovieGrid.js
+++ src/components/MovieGrid.js
@@ -5,6 +5,8 @@ import MovieCard from './MovieCard';
 export default function MovieGrid() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [genre, setGenre] = useState('All Genres');
+  const [rating, setRating] = useState('All');
 
   useEffect(() => {
     fetch('movies.json')
@@ -29,6 +31,27 @@ export default function MovieGrid() {
         value={searchTerm}
         onChange={handleSearchChange}
       />
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label htmlFor="">Genre</label>
+          <select className="filter-dropdown" name="" id="">
+            <option value="">All Genres</option>
+            <option value="">Action</option>
+            <option value="">Drama</option>
+            <option value="">Fantasy</option>
+            <option value="">Horror</option>
+          </select>
+        </div>
+        <div className="filter-slot">
+          <label htmlFor="">Rating</label>
+          <select className="filter-dropdown" name="" id="">
+            <option value="">All</option>
+            <option value="">Good</option>
+            <option value="">Ok</option>
+            <option value="">Bad</option>
+          </select>
+        </div>
+      </div>
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
